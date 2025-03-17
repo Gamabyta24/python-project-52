465e3e206a2db543e1ae40db9372e475c7d08a24
Gamabyta24 <Gamabyta@gmail.com>
2025-03-17 20:40:29 +0300

    подготовка к деплою

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6i_v01dg)-sg^&z8p76+%@4&g@n#t*wwnt%s8t^+oo-m0*lxge
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['webserver','127.0.0.1']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'task_manager',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'task_manager.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
