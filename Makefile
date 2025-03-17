37ced86a402c8c9705062a31df14f186386fe137
Gamabyta24 <Gamabyta@gmail.com>
2025-03-17 22:45:47 +0300

    Поправил файл сборки

--- Makefile
+++ Makefile
@@ -4,7 +4,5 @@ render-start:
 	gunicorn task_manager.wsgi
 install:
 	uv sync
-collectstatic:
-	python manage.py collectstatic --no-input
 migrate:
 	python manage.py migrate
--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 curl -LsSf https://astral.sh/uv/install.sh | sh
 source $HOME/.local/bin/env
-make install && make collectstatic && make migrate
+make install && make migrate
